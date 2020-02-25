a1efed5943107f77cbd6fcd21fa6050be3e8d17b
florenzliu <fangliu_connie@hotmail.com>
2020-02-25 16:47:53 +0000

    Completed sumDigits

--- main.scm
+++ main.scm
@@ -1,38 +1,19 @@
-(define (celcius->farenheit celcius)
-  (+ (* 1.8 celcius) 32))
-;(display (celcius->farenheit 0)) (newline)
-;(display (celcius->farenheit 100)) (newline)
+; Question 1 sumDigits
+(define (sumDigits s)
+  (sumDigitsHelper (string->list s)))
 
-(define (factorial n)
-  (if (zero? n) 1
-    (* n (factorial (- n 1)))))
+(define (sumDigitsHelper ls)    
+  (if (null? ls) 0
+    (+ (numeric? (car ls)) 
+    (sumDigitsHelper (cdr ls)))))
 
-;(display(factorial 0)) (newline)
-;(display(factorial 4)) (newline)
-;(display(factorial 10)) (newline)
+(define (numeric? ch)
+  (if (and (char<=? #\0 ch) (char>=? #\9 ch)) (- (char->integer ch) (char->integer #\0))
+  0))
 
-(define (fibonacci n)
-  (if (< n 2) n
-    (+ (fibonacci (- n 2))
-    (fibonacci (- n 1)))))
-;(display (fibonacci 10))
-;(newline)
+;(display (sumDigits "ab3nslg40adgv")) (newline)
+;(display (sumDigits "")) (newline)
+;(display (sumDigits "il0gv12d5f2")) (newline)
+(display (sumDigits "ab1c2d3e54")) (newline)
 
-
-;(define (isDigit ch)
- ; (if (integer? (char->integer ch)) 1
-  ;  0))
-;(isDigit '3')
-
-;(define (sumDigits s)
- ; (cond ((null? s) 0)
-  ;      ((integer? (char->integer (car s))) (+ (char->integer (car s)) (sumDigits (cdr s))))
-   ;     (else (sumDigits (cdr s)))))
-;(sumDigits "a1")   
-  ;  (+ (fibonacci (- n 2))
-   ; (fibonacci (- n 1)))))
-
-
-;(define (less_than a b)
-;  (if (char<=? a b) #t
-;      #f))
+; Question 2 
