dea3a9004c1023ec683f6c63a43bf60d7e2405eb
florenzliu <fangliu_connie@hotmail.com>
2020-02-26 00:20:20 +0000

    Attempted Q2 -- not working

--- main.scm
+++ main.scm
@@ -17,3 +17,43 @@
 (display (sumDigits "ab1c2d3e54")) (newline)
 
 ; Question 2 
+#|
+(define lsCount ())
+(define (countCodesHelper ls)
+  (if (null? ls) 0
+    (if (string=? (car ls) str)
+      (+ 1 (countCodesHelper (cdr ls)))
+      (countCodesHelper (cdr ls)))))
+(display (countCodesHelper lst)) (newline)
+|#
+
+
+(define (countCodes s)
+  (if (null? s) 0
+    ((define lsCount ())
+    (countCodesHelper lsCount (string->list s)))))
+#|
+(define (isInCountStr? lsCount ch1 ch2) 
+  (if (null? lsCount) #f
+    (if (string=? (car lsCount) (string ch1 ch2)) #t 
+        (isInCountStr? (cdr lsCount) ch1 ch2)))) 
+|#
+
+(define (addToCount lsCount ch1 ch2) 
+  (if (null? lsCount) #f
+    (if (string=? (car lsCount) (string ch1 ch2)) #t 
+        (addToCount (cdr lsCount) ch1 ch2)))) 
+
+(define (countCodesHelper lsCount ls)
+  (if (null? ls) 0
+    (if (null? (car (cdr ls))) 0
+        (if (addToCount lsCount (car ls) (car (cdr ls))) 
+          ((set! lsCount (append lsCount (list (string (car ls) (car (cdr ls)))))) (display lsCount) (newline)
+          (+ 1 (countCodesHelper lsCount (cdr ls))))))))
+
+(display " finished here") (newline)
+
+(define lst (list "AB" "IN" "CD" "IN"))
+(define countryCode (list "IN" "CO" "CN"))
+(display lst) (newline)
+(display (countCodes "ABICDONINBIN" countryCode)) (newline)
