e77f122a5323a4fc2cdfd1dfd20e37e3249f5356
florenzliu <fangliu_connie@hotmail.com>
2020-02-28 05:46:20 +0000

    Updated country code in Q2

--- main.scm
+++ main.scm
@@ -19,7 +19,8 @@
 
 ; Question 2 
 ; define a reference list with all the country codes
-(define countryCode (list "IN" "CO" "CN" "GB"))
+(define countryCode (list "AD" "AE" "AF" "AG" "AI" "AL" "AM" "AO" "AQ" "AR" "AS" "AT" "AU" "AW" "AX" "AZ" "BA" "BB" "BD" "BE" "BF" "BG" "BH" "BI" "BJ" "BL" "BM" "BN" "BO" "BQ" "BR" "BS" "BT" "BV" "BW" "BY" "BZ" "CA" "CC" "CD" "CF" "CG" "CH" "CI" "CK" "CL" "CM" "CN" "CO" "CR" "CU" "CV" "CW" "CX" "CY" "CZ" "DE" "DJ" "DK" "DM" "DO" "DZ" "EC" "EE" "EG" "EH" "ER" "ES" "ET" "FI" "FJ" "FK" "FM" "FO" "FR" "GA" "GB" "GD" "GE" "GF" "GG" "GH" "GI" "GL" "GM" "GN" "GP" "GQ" "GR" "GS" "GT" "GU" "GW" "GY" "HK" "HM" "HN" "HR" "HT" "HU" "ID" "IE" "IL" "IM" "IN" "IO" "IQ" "IR" "IS" "IT" "JE" "JM" "JO" "JP" "KE" "KG" "KH" "KI" "KM" "KN" "KP" "KR" "KW" "KY" "KZ" "LA" "LB" "LC" "LI" "LK" "LR" "LS" "LT" "LU" "LV" "LY" "MA" "MC" "MD" "ME" "MF" "MG" "MH" "MK" "ML" "MM" "MN" "MO" "MP" "MQ" "MR" "MS" "MT" "MU" "MV" "MW" "MX" "MY" "MZ" "NA" "NC" "NE" "NF" "NG" "NI" "NL" "NO" "NP" "NR" "NU" "NZ" "OM" "PA" "PE" "PF" "PG" "PH" "PK" "PL" "PM" "PN" "PR" "PS" "PT" "PW" "PY" "QA" "RE" "RO" "RS" "RU" "RW" "SA" "SB" "SC" "SD" "SE" "SG" "SH" "SI" "SJ" "SK" "SL" "SM" "SN" "SO" "SR" "SS" "ST" "SV" "SX" "SY" "SZ" "TC" "TD" "TF" "TG" "TH" "TJ" "TK" "TL" "TM" "TN" "TO" "TR" "TT" "TV" "TW" "TZ" "UA" "UG" "UM" "US" "UY" "UZ" "VA" "VC" "VE" "VG" "VI" "VN" "VU" "WF" "WS" "YE" "YT" "ZA" "ZM" "ZW"))
+
 ; define a result list with non-repetitive items
 (define resStrList (list ))
 
@@ -53,7 +54,7 @@
         (else (if (string=? ns (car resList)) #t
                 (isInResList? ns (cdr resList))))))
 
-(display (countCodes "CNCNCNCNCCCN")) (newline)
+;(display (countCodes "AINBCO")) (newline)
 
 ; Question 3
 ; define a list with unique characters
@@ -71,7 +72,7 @@
         (set! uniqueCharList (list ))
         ; update the maxLen by calling currUniqSubLen function
         (set! maxLen (max maxLen (currUniqSubLen ls)))
-        (max maxLen (uniqueSubstringHelper (cdr ls))))))
+        (max maxLen (uniqueSubstringHelper (cdr ls)))))
 
 ; return the length of the substring with unique characters that starts from first character of the list
 (define (currUniqSubLen ls)
